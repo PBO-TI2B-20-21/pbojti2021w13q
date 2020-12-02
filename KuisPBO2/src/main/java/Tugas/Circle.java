8b93ae5c0209b8ce54b8f97e7697dcf1cbcdb6a9
RizalWhisnu <rizalwhisnu00@gmail.com>
2020-12-02 15:01:24 +0700

    Kuis2_1941720227_RizalWhisnu

---
+++ KuisPBO2/src/main/java/Tugas/Circle.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author hp
+ */
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle() {
+        this.radius = 3.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public String toString() {
+        return "Circle[Shape" + "[color=" + getColor() + "," + "filled=" + isFilled() + "]," + "radius=" + radius + "]";
+    }
+}
